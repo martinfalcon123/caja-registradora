e37592e23582c61f2eecb52bedde3560809d81fd
martin falcon <martinfalcon2020@gmail.com>
2025-05-28 15:44:06 -0300

    haciendo pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ export default function App() {
   return (
     <div style={{ maxWidth: "900px", margin: "2rem auto", padding: "0 1rem" }}>
       <h1 style={{ textAlign: "center", color: "#317773", marginBottom: "2rem" }}>
-        Tienda de Ropa
+        Mi primera APP
       </h1>
 
       <ProductList products={products} onAddToCart={handleAddToCart} />
